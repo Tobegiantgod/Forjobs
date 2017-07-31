4932723e0463b8d07cca3d23870c265b663ee1d9
ChinaWin <hainuzsr@gmail.com>
2017-07-31 09:11:57 -0400

    updated ProcessPoolExecutor.py

--- ProcessPoolExecutor_learn.py
+++ ProcessPoolExecutor_learn.py
@@ -1,13 +1,17 @@
 #coding: utf-8
 """用concurrent.futures的ProcessPoolExecutor调用multiprocess，实现真正的并行"""
 
-""" multiprocess的开销较大，原因在于：主进程与子进程之间，必须进行序列化与反序列化操作。
-    程序中的大量开销正是由于这些操作所引发的。所以用ProcessPoolExecutor类来使用多进程
-    multiprocess时，更适合于那些只需要小部分数据传输就可以进行大量计算的操作。当这样不
-    能提高效率时，再考虑使用multiprocessing模块中的复杂特性。"""
+""" 
+    multiprocess的开销较大，原因在于：主进程与子进程之间，必须进行序列化与反序列化
+操作。程序中的大量开销正是由于这些操作所引发的。所以用ProcessPoolExecutor类来使用
+多进程multiprocess时，更适合于那些只需要小部分数据传输就可以进行大量计算的操作。当
+这样不能提高效率时，再考虑使用multiprocessing模块中的复杂特性。
+"""
 
 from concurrent.futures import ProcessPoolExecutor
 from time import time
+
+
 def gcd(pair):
     a, b = pair
     low = min(a, b)
@@ -15,13 +19,14 @@ def gcd(pair):
         if a % i == 0 and b % i == 0:
             return  i
 
+#单进程
 numbers = [(13456316,4564732), (45664684,45654654), (456456548,2354654)]
 start1 = time()
 results1 = list(map(gcd, numbers))
 end1 = time()
 print('single process took %.3f seconds' % (end1-start1))
 
-
+#多进程
 start2 = time()
 pool = ProcessPoolExecutor(max_workers=2)
 results2 = list(pool.map(gcd, numbers))
