e8d0b03fac721205e1fb4fa05040b16c1235bd9a
ChinaWin <hainuzsr@gmail.com>
2017-08-01 20:01:51 -0400

    changed tcp_server.py

--- ProcessPoolExecutor_learn.py
+++ ProcessPoolExecutor_learn.py
@@ -1,4 +1,4 @@
-#coding: utf-8
+# coding: utf-8
 """用concurrent.futures的ProcessPoolExecutor调用multiprocess，实现真正的并行"""
 
 """ 
@@ -15,18 +15,20 @@ from time import time
 def gcd(pair):
     a, b = pair
     low = min(a, b)
-    for i in range(low, 0 ,-1):
+    for i in range(low, 0, -1):
         if a % i == 0 and b % i == 0:
-            return  i
+            return i
 
-#单进程
-numbers = [(13456316,4564732), (45664684,45654654), (456456548,2354654)]
+# 单进程
+
+
+numbers = [(13456316, 4564732), (45664684, 45654654), (456456548, 2354654)]
 start1 = time()
 results1 = list(map(gcd, numbers))
 end1 = time()
 print('single process took %.3f seconds' % (end1-start1))
 
-#多进程
+# 多进程
 start2 = time()
 pool = ProcessPoolExecutor(max_workers=2)
 results2 = list(pool.map(gcd, numbers))
